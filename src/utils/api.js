790b75fe55d7ab1ca706177c2d10257c9dd63d86
Medmixal <medmixal@yahoo.com>
2023-02-10 11:35:42 +0400

    make likes show

---
+++ src/utils/api.js
@@ -0,0 +1,55 @@
+const onResponce = (res) => {
+    return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+}
+
+class Api {
+    constructor({baseUrl, headers}){
+        this._headers = headers;
+        this._baseUrl= baseUrl;
+    }
+
+    getProductList() {
+        return fetch(`${this._baseUrl}/products`, {
+            headers: this._headers
+        }).then(onResponce)
+    }
+
+    getUserInfo() {
+        return fetch(`${this._baseUrl}/users/me`, {
+            headers: this._headers
+        }).then(onResponce)
+    }
+
+    setUserInfo(dataUser) {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify(dataUser)
+        }).then(onResponce)
+    }
+
+    search(searchQuery) {
+        return fetch(`${this._baseUrl}/products/search?query=${searchQuery}`, {
+            headers: this._headers
+        }).then(onResponce)
+    }
+
+    changeLikeProduct(productId, isLike) {
+        return fetch(`${this._baseUrl}/products/likes/${productId}`, {
+            method: isLike ? "DELETE" : "PUT",
+            headers: this._headers
+        }).then(onResponce)
+    }
+}
+
+const config = {
+    baseUrl: 'https://api.react-learning.ru',
+    headers: {
+        'content-type': 'application/json',
+        Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2UxMjY1NzU5Yjk4YjAzOGY3N2IyMGYiLCJncm91cCI6Imdyb3VwLTEwIiwiaWF0IjoxNjc1NzAwMTg5LCJleHAiOjE3MDcyMzYxODl9.3g3J4NWBJWJdqe1QKr_aLIBf_VMKyo0r0dkEr04l7EE'
+    }
+}
+
+const api = new Api(config);
+
+export default api;
