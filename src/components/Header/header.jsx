44f096fd50699f4ee381fe5e9fcfcb3f143c8f3f
Medmixal <medmixal@yahoo.com>
2023-02-24 17:47:21 +0400

    make sort, favorite page, 404 page, product page

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,9 +1,12 @@
 import s from './index.module.css';
 import cn from 'classnames';
-
+import { CardContext } from '../../context/cardContext';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import {ReactComponent as FavoriteIcon} from './img/favorites.svg';
 
 function Header({children, user, onUpdateUser}) {
-
+  const { favorites } = useContext(CardContext);
   const handleClickButtonEdit = (e)=> {
     e.preventDefault();
     onUpdateUser({name: 'Максим', about: 'Ментор'})
@@ -18,8 +21,14 @@ function Header({children, user, onUpdateUser}) {
 
           <button className='btn' onClick={handleClickButtonEdit}>Изменить</button>
         </div>
-        <div className={s.wrapper}>
+        <div className={s.header__wrapper}>
           {children}
+          <div className={s.iconsMenu}>
+            <Link className={s.favoritesLink} to={{pathname:"/favorites", state: 'sfsdfsdf'}}>
+              <FavoriteIcon/>
+              {favorites.length !== 0 && <span className={s.iconBubble}>{favorites.length}</span>}
+            </Link>
+          </div>
         </div>
       </div>
     </header>
