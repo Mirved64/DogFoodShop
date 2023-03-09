0b3e1df93db485d22de3bff4629cdf9cd9ab703e
Medmixal <medmixal@yahoo.com>
2023-03-09 09:57:14 +0400

    some change

--- src/pages/FavoritePage/favorite-page.jsx
+++ src/pages/FavoritePage/favorite-page.jsx
@@ -1,20 +1,19 @@
 import { useContext } from "react";
-import CardList from "../../components/CardList/card-list"
+import CardList from "../../components/CardList/card-list";
 import { ContentHeader } from "./../../components/ContentHeader/content-header";
-import Sort from "../../components/Sort/sort"
-import Spinner from "../../components/Spinner"
+import Sort from "../../components/Sort/sort";
+
 import { CardContext } from "../../context/cardContext";
 
-
 export const FavoritePage = () => {
-    const { favorites } = useContext(CardContext);
-    return (
-        <>
-            <ContentHeader title="Избранное"/>
-            <Sort />
-            <div className='content__cards'>
-                <CardList cards={favorites}/>
-            </div>
-        </>
-    )
-}
+  const { favorites } = useContext(CardContext);
+  return (
+    <>
+      <ContentHeader title="Избранное" />
+      <Sort />
+      <div className="content__cards">
+        <CardList cards={favorites} />
+      </div>
+    </>
+  );
+};
